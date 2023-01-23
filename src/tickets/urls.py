29f5ad8833ca01f441f94236793691c9069d2306
vmelfx <vladimir.meleshchenko@outllok.com>
2023-01-23 22:21:35 +0200

    Django base implementation
    - Add serializers.py for data handling
    - Add trash folder for current code examples

--- src/tickets/urls.py
+++ src/tickets/urls.py
@@ -1,14 +1,35 @@
+from django.http import JsonResponse
 from django.urls import path
+from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView
 from tickets.models import Ticket
-from tickets.serializers import TicketSerializer
+from tickets.serializers import (
+    TicketCreateSerializer,
+    TicketLightSerializer,
+    TicketSerializer,
+)
 
 
 class TicketsGet(ListAPIView):
     queryset = Ticket.objects.all()
-    serializer_class = TicketSerializer
+    serializer_class = TicketLightSerializer
+
+
+def get_ticket(request, id_: int) -> JsonResponse:
+    ticket: Ticket = Ticket.objects.get(id=id_)
+    serializer = TicketSerializer(ticket)
+    return JsonResponse(serializer.data)
+
+
+@api_view(["POST"])
+def create_ticket(request) -> JsonResponse:
+    serializer = TicketCreateSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    return JsonResponse(serializer.data)
 
 
 urlpatterns = [
-    path("", TicketsGet.as_view()),
+    path("/", TicketsGet.as_view()),
+    path("/create", create_ticket),
+    path("/<int:id_>", get_ticket),
 ]
