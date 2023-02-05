6498a636455b1842d75e0786126dff2652698dd9
vmelfx <vladimir.meleshchenko@outllok.com>
2023-02-05 20:36:45 +0200

    Minor fixes in settings.py syntax

--- src/config/settings.py
+++ src/config/settings.py
@@ -112,7 +112,6 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
     ),
-
     "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
