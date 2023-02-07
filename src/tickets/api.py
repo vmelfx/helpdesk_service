35bca533749a1f69e886ba49ed0ee5c5e944f86e
vmelfx <vladimir.meleshchenko@outllok.com>
2023-02-07 02:28:57 +0200

    Started with permissions for tickets
    - Created permissions.py for storing permissions
    - Adjusted src\tickets\api.py to be able to use permissions.

--- src/tickets/api.py
+++ src/tickets/api.py
@@ -1,22 +1,76 @@
+from django.db.models.query import QuerySet
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from shared.serializers import ResponseMultiSerializer, ResponseSerializer
 from tickets.models import Ticket
+from tickets.permissions import IsOwner, RoleIsAdmin, RoleIsManager, RoleIsUser
 from tickets.serializers import TicketLightSerializer, TicketSerializer
 
 
-class TicketAPISet(ViewSet):
+class TicketAPISet(ViewSet, GenericAPIView):
+    queryset = Ticket.objects.all()
+
+    def get_permissions(self):
+        if self.action == "list":
+            permission_classes = [RoleIsAdmin]
+        elif self.action == "create":
+            permission_classes = [RoleIsUser]
+        elif self.action == "update":
+            permission_classes = [RoleIsManager]
+        elif self.action == "retrieve":
+            permission_classes = [IsOwner | RoleIsAdmin | RoleIsManager]
+        elif self.action == "destroy":
+            permission_classes = [RoleIsAdmin]
+        else:
+            permission_classes = []
+
+        return [permission() for permission in permission_classes]
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            "Expected view %s to be called with a URL keyword argument "
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            "attribute on the view correctly." % (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def get_queryset(self):
+        assert self.queryset is not None, (
+            "'%s' should either include a `queryset` attribute, "
+            "or override the `get_queryset()` method." % self.__class__.__name__
+        )
+
+        queryset = self.queryset
+        if isinstance(queryset, QuerySet):
+            # Ensure queryset is re-evaluated on each request.
+            queryset = queryset.all()
+        return queryset
+
     def list(self, request):
-        queryset = Ticket.objects.all()
+        queryset = self.get_queryset()
         serializer = TicketLightSerializer(queryset, many=True)
         response = ResponseMultiSerializer({"results": serializer.data})
 
         return Response(response.data)
 
     def retrieve(self, request, pk: int):
-        instance = Ticket.objects.get(id=pk)
+        instance: Ticket = self.get_object()
         serializer = TicketSerializer(instance)
         response = ResponseSerializer({"result": serializer.data})
 
@@ -34,7 +88,7 @@ class TicketAPISet(ViewSet):
         return JsonResponse(response.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, pk: int):
-        instance = Ticket.objects.get(id=pk)
+        instance: Ticket = self.get_object()
 
         context: dict = {
             "request": self.request,
@@ -48,6 +102,7 @@ class TicketAPISet(ViewSet):
         return JsonResponse(response.data)
 
     def destroy(self, request, pk: int):
-        Ticket.objects.get(id=pk).delete()
+        instance: Ticket = self.get_object()
+        instance.delete()
 
         return JsonResponse({}, status=status.HTTP_204_NO_CONTENT)
---
+++ src/tickets/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework.permissions import BasePermission
+from users.schemas import Role
+
+
+class RoleIsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == Role.ADMIN
+
+
+class RoleIsUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == Role.USER
+
+
+class RoleIsManager(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == Role.MANAGER
+
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.customer == request.user
